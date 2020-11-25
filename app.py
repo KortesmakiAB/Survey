1643a8caedca5dc958188fd4ca1fac2758784206
Aaron Brant <aaronbrant1@gmail.com>
2020-11-25 12:35:39 -0500

    initial stage six commits

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -9,6 +9,7 @@ debug = DebugToolbarExtension(app)
 
 responses = []
 idx = 1
+counter = 1
 
 @app.route('/')
 def show_homepage():
@@ -18,13 +19,24 @@ def show_homepage():
 @app.route('/questions/<int:idx>')
 def handle_questions(idx):
     """When the user arrives at one of these pages, it should show a form asking the current question, and listing the choices as radio buttons."""
-    
-    answer = request.args.get("answer", None)
-    responses.append(answer)
+    global counter
 
-    if idx - 1 < len(satisfaction_survey.questions):
-        idx += 1
+    # questions/1 doesn't have an answer, so don't inlclude "None" in responses
+    if idx > 1:
+        answer = request.args.get('answer', None)
+        responses.append(answer)
+
+    # If user attempts to change url: user is unable to go backwards, user is unable to go forwards by 2 or more. But user can go forward 1.
+    # What needs to be different so that if user increments url by 1, they will be redirected back to counter.
+    path = request.path
+    if request.path == f"/questions/{counter}":
+        if idx - 1 < len(satisfaction_survey.questions):
+            idx += 1
+            counter += 1
+            return render_template('questions.html', survey=satisfaction_survey, idx=idx, path=path, counter=counter)
+        else:
+            return render_template('thank-you.html', responses=responses)
     else:
-        return render_template('thank-you.html')
-    return render_template('questions.html', survey=satisfaction_survey, idx=idx-1)
-
+        counter -= 1
+        return redirect(f'/questions/{counter}')          
+ 
